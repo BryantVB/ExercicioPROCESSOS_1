cf576064fb3ebcc9996e1d5657b9ead04cb2122e
Bryan <Bryan@DESKTOP-O0TJ40G>
2022-03-02 17:01:58 -0300

    EXERCICIO 1

--- src/view/PRINCIPAL.java
+++ src/view/PRINCIPAL.java
@@ -1,10 +1,24 @@
 package view;
+import javax.swing.JOptionPane;
 
-public class PRINCIPAL {
+import controller.redecontroller;;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class PRINCIPAL  {
+    public static void main(String[] args) {
+        redecontroller controller = new redecontroller();
+        int option = 0;
+        do {
+            option = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Mostrar Adaptadores com IPv4\n"+
+                                              "2 - Média de ping google\n"+
+                                              "3 - Sair"));
+            switch(option) {
+                case 1:
+                    controller.ip();break;
+                case 2:
+                    controller.ping();break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Finalizando.");break;
+            }
+        } while(option != 3);
+    }
 }
